376b7c8140a3d87dd3aa45a4a1526d7f1cee2957
David Santoso <david.e.santoso@gmail.com>
2013-12-03 13:20:26 -0600

    Updating folder references

--- lib/markety.rb
+++ lib/markety.rb
@@ -1,15 +1,11 @@
 require 'rubygems'
 require 'savon'
 
-Savon.configure do |config|
-  config.log = false # disable logging
-end
-
-require File.expand_path('marketo/client', File.dirname(__FILE__))
-require File.expand_path('marketo/authentication_header', File.dirname(__FILE__))
-require File.expand_path('marketo/enums', File.dirname(__FILE__))
-require File.expand_path('marketo/lead_key', File.dirname(__FILE__))
-require File.expand_path('marketo/lead_record', File.dirname(__FILE__))
+require File.expand_path('markety/client', File.dirname(__FILE__))
+require File.expand_path('markety/authentication_header', File.dirname(__FILE__))
+require File.expand_path('markety/enums', File.dirname(__FILE__))
+require File.expand_path('markety/lead_key', File.dirname(__FILE__))
+require File.expand_path('markety/lead_record', File.dirname(__FILE__))
 
 
 
