41550677d6aa71f603803d1528245c363e935425
David Santoso <david.e.santoso@gmail.com>
2014-04-21 01:04:42 -0500

    Basic client test

--- spec/markety/client_spec.rb
+++ spec/markety/client_spec.rb
@@ -1,5 +1,10 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 module Markety
-
+  describe Client do
+    it 'should instantiate with a Savon client and authentication header' do
+      client = Client.new(double('savon_client'), double('authentication_header'))
+      client.class.should == Markety::Client
+    end
+  end
 end
