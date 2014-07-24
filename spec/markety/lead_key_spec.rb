75f32a0b46aef6ea95681b92ddf266006dab7333
Grant Birchmeier <gbirchmeier@connamara.com>
2014-07-24 12:09:13 -0500

    don't reassign constants (to avoid annoying warning message when running tests)

--- spec/markety/lead_key_spec.rb
+++ spec/markety/lead_key_spec.rb
@@ -16,17 +16,16 @@ module Markety
   end
 
   describe LeadKey do
+    KEY_VALUE = 'a value'
+    KEY_TYPE = LeadKeyType::IDNUM
+
     it "should store type and value on construction" do
-      KEY_VALUE = 'a value'
-      KEY_TYPE = LeadKeyType::IDNUM
       lead_key = LeadKey.new(KEY_TYPE, KEY_VALUE)
       lead_key.key_type.should == KEY_TYPE
       lead_key.key_value.should == KEY_VALUE
     end
 
     it "should to_hash correctly" do
-      KEY_VALUE = 'a value'
-      KEY_TYPE = LeadKeyType::IDNUM
       lead_key = LeadKey.new(KEY_TYPE, KEY_VALUE)
 
       lead_key.to_hash.should == {
