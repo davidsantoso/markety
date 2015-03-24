6e666fd9eff49ec64d5d3edb11bf64f5e4310e35
Mark <mark.a@serviceseeking.com.au>
2015-03-24 17:26:58 +1100

    refactored get custom object response"

--- lib/markety/response/get_custom_object_response.rb
+++ lib/markety/response/get_custom_object_response.rb
@@ -1,22 +1,38 @@
 module Markety
   module Response
     class GetCustomObjectResponse < GenericResponse
-      attr_accessor :custom_objects
       def initialize(response)
         super(:get_custom_object_response, response)
-        @custom_objects = []
-        object_type_name = to_hash[:success_get_custom_objects][:result][:obj_type_name]
-        custom_object_list.each do |custom_obj_hash|
-          @custom_objects << CustomObject.from_marketo_hash(object_type_name, custom_obj_hash)
-        end
       end
 
-      def custom_object_list
-        [(to_hash[:success_get_custom_objects][:result][:custom_obj_list] || {})[:custom_obj] || []].flatten
+      def custom_objects
+        @custom_objects ||= begin
+          custom_object_list.map do |custom_obj_hash|
+            CustomObject.from_marketo_hash(object_type_name, custom_obj_hash)
+          end
+        end
       end
 
       def custom_object
         custom_objects.first
+      end
+
+      private
+
+      def custom_object_list
+        [custom_object_list_hash.fetch(:custom_obj, [])].flatten
+      end
+
+      def custom_object_list_hash
+        result_hash[:custom_obj_list] || {}
+      end
+
+      def object_type_name
+        result_hash.fetch(:obj_type_name, {})
+      end
+
+      def result_hash
+        to_hash.fetch(:success_get_custom_objects, {}).fetch(:result, {})
       end
     end
   end
